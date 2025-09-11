8c0ca7ce6a3c704d6484f978d7b39d1f23c42240
prastikaac <acharyaprasiddha6@gmail.com>
2025-09-11 22:50:01 +0300

    sdfds

--- js/popupLogic.js
+++ js/popupLogic.js
@@ -993,7 +993,7 @@ window.addEventListener("load", () => {
         // Show popupStep1 after 5 seconds of user on the page after load
         setTimeout(() => {
           showPopupStep("popupStep1")
-        }, 8000)
+        }, 1000)
       } else {
         closePopup()
       }
